c67a7354cbbb917d190198ae947017aef33bce68
ariana <ariana.daney@ssfs.org>
2019-10-17 11:19:54 -0400

    started daily work

--- daily_exercises.py
+++ daily_exercises.py
@@ -2,3 +2,13 @@
 # last modified october 17, 2019
 # daily exercises unit 5
 
+# ex. 1
+
+for x in range(21):
+    print(x)
+
+# ex. 2
+
+user_choice = int(input("enter a number please"))
+for x in range(1, user_choice + 1):
+    print(x)
