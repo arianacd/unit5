aac30e9051bcfa3b0317ae89845e4aaff8ba1ccb
ariana <ariana.daney@ssfs.org>
2019-10-18 12:05:30 -0400

    did daily work ex. 1 -4

--- daily_exercises.py
+++ daily_exercises.py
@@ -2,13 +2,43 @@
 # last modified october 17, 2019
 # daily exercises unit 5
 
+import random
 # ex. 1
 
-for x in range(21):
-    print(x)
+# print("make sure the numbers you enter make sense in order")
+# user_upper = int(input("enter a upper number please"))
+# user_lower = int(input("please enter a lower number"))
+# for x in range(user_lower, user_upper + 1):
+#     print(x)
+# if user_lower > user_upper:
+#     for x in range(user_lower, user_upper - 1, - 1):
+#         print(x)
+
 
 # ex. 2
+#
+# for x in range(10, 21, 2):
+#     print(x)
+#
 
-user_choice = int(input("enter a number please"))
-for x in range(1, user_choice + 1):
-    print(x)
+# ex. 3
+# user_even = int(input("please enter a positive number"))
+# for x in range(13):
+#     print(user_even, "*", x, "=", user_even * x)
+
+
+# ex. 4
+# even_sum = 0
+# odd_sum = 0
+# for x in range(11):
+#     num = random.randint(1, 101)
+#     print(num)
+#     if (num % 2) == 0:
+#         even_sum = even_sum + num
+#     else:
+#         odd_sum = odd_sum + num
+# print("the even numbers add to", even_sum)
+# print("the odd numbers add to", odd_sum)
+
+
+# ex. 5
